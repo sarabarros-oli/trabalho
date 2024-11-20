151b436c64e046d6200ac39207a92e24ed955697
Sara <saraholiveiraptu@gmail.com.br>
2024-11-19 21:39:58 -0300

    Alteração em Adicionar Tarefas

--- src/main/java/com/mycompany/trabalho/AdicionarTarefas.java
+++ src/main/java/com/mycompany/trabalho/AdicionarTarefas.java
@@ -20,22 +20,51 @@ public class AdicionarTarefas {
         this.tarefas = tarefas;
     }
    
-    public void adicionar(){
+   public void adicionar() {
         Tarefa ta = new Tarefa();
-        System.out.println("Titulo");
+
+        System.out.println("Título:");
         ta.setTitulo(sc.nextLine());
-        System.out.println("Descricao");
+
+        System.out.println("Descrição:");
         ta.setDescricao(sc.nextLine());
-        System.out.println("Data Vencimento");
+
+        System.out.println("Data de Vencimento (formato yyyy-MM-dd):");
         String dataVencimento = sc.nextLine();
-        ta.setDataVencimento(LocalDate.MIN);
-        System.out.println("Status");
-        String status=sc.nextLine(); // Consumir o caractere de nova linha pendente, se necessário
-        ta.setStats(Tarefa.Stat.PENDENTE);
+        ta.setDataVencimento(LocalDate.parse(dataVencimento));
+
+        Tarefa.Stat status = escolherStatus();
+        ta.setStats(status);
+
         tarefas.add(ta);
-        /*System.out.println("Tarefa: " + tarefas.size());
-        for(Tarefa t: tarefas){
-            System.out.println("Titulo"+ t.getTitulo());
-        }*/
+
+        System.out.println("Tarefa adicionada com sucesso!");
+        for (Tarefa t : tarefas) {
+            System.out.println("\nTítulo: " + t.getTitulo());
+            System.out.println("Descrição: " + t.getDescricao());
+            System.out.println("Data de Vencimento: " + t.getDataVencimento());
+            System.out.println("Status: " + t.getStats());
+        }
+    }
+     private Tarefa.Stat escolherStatus() {
+        System.out.println("Escolha o status da tarefa:");
+        Tarefa.Stat[] statuses = Tarefa.Stat.values();
+
+        for (int i = 0; i < statuses.length; i++) {
+            System.out.println((i + 1) + ". " + statuses[i]);
+        }
+
+        int escolha;
+        while (true) {
+            System.out.print("Digite o número correspondente ao status: ");
+            escolha = sc.nextInt();
+            sc.nextLine(); // Consumir o caractere de nova linha pendente
+
+            if (escolha > 0 && escolha <= statuses.length) {
+                return statuses[escolha - 1];
+            } else {
+                System.out.println("Opção inválida. Tente novamente.");
+            }
+        }
     }
 }
