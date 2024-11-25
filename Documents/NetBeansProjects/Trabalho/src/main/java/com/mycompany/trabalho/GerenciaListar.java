91d497627acfc3a3c783528e2918d1d737a6eba2
Tainara Pereira <tainara61cec@gmail.com>
2024-11-25 12:34:24 -0300

    adiçãoo de filtro nas buscas e criação da classe enum

--- Documents/NetBeansProjects/Trabalho/src/main/java/com/mycompany/trabalho/GerenciaListar.java
+++ Documents/NetBeansProjects/Trabalho/src/main/java/com/mycompany/trabalho/GerenciaListar.java
@@ -5,6 +5,10 @@
 package com.mycompany.trabalho;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Scanner;
 
 /**
  *
@@ -17,7 +21,8 @@ public class GerenciaListar {
         this.tarefas = tarefas;
     }
     
-    public void inicial(){
+    public void listarNomal(){
+       
         for(Tarefa tar :tarefas){
             System.out.println("TAREFA Nº "+ tarefas.size()+1);
             System.out.println("Titulo: " + tar.getTitulo());
@@ -26,5 +31,35 @@ public class GerenciaListar {
             System.out.println("Data de Vencimento:" + tar.getDataVencimento());
         }
         }
-   
+   public void listarStatus(){
+       int op;
+       Scanner sc = new Scanner(System.in);
+       System.out.println("Escolha o Status: 1- Pendente 2- Em andamento 3- Concluido");
+       op = sc.nextInt();
+       for(Tarefa tar :tarefas){
+           if(tar.getStats() == Stat.values()[op]){
+               System.out.println("TAREFA Nº "+ tarefas.size()+1);
+           System.out.println("Titulo: " + tar.getTitulo());
+            System.out.println("Descrição: "+ tar.getDescricao());
+            System.out.println("Status:" + tar.getStats());
+            System.out.println("Data de Vencimento:" + tar.getDataVencimento());
+           }
+            
+        }
+   }
+  public void listarData(){
+        ArrayList<Tarefa> tarefasOrdenadas = new ArrayList<>();
+        Collections.copy(tarefasOrdenadas, tarefas);
+
+        // Ordenar o novo ArrayList por data de vencimento
+        Collections.sort(tarefasOrdenadas, Comparator.comparing(Tarefa::getDataVencimento));
+         for(Tarefa tar :tarefasOrdenadas){
+            System.out.println("TAREFA Nº "+ tarefas.size()+1);
+           System.out.println("Titulo: " + tar.getTitulo());
+            System.out.println("Descrição: "+ tar.getDescricao());
+            System.out.println("Status:" + tar.getStats());
+            System.out.println("Data de Vencimento:" + tar.getDataVencimento());
+           }
+      
+  } 
 }
